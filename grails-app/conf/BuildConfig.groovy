b7550166b93075e50589eb95acb351e1b75e3621
Alidad <programmer@dorisa.com>
2013-07-21 18:12:36 -0500

    adding the plugin compile ":rest-client-builder:1.0.2"

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,6 +45,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+        compile ":rest-client-builder:1.0.2"
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
