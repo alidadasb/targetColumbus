b275a84248e62ea8937b822c84340450371afd25
dmahapatro <dmahapatro@netjets.com>
2013-07-21 20:48:11 -0400

    Store service class

---
+++ grails-app/services/com/target/stores/StoreService.groovy
@@ -0,0 +1,22 @@
+package com.target.stores
+
+import static com.target.constants.Target.*
+
+class StoreService {
+    def rest
+
+    def storesByZipCode(Map searchCriteria) {
+        //https://api.target.com/v2/store?nearby=43230&key=fzg8mSgoVXUxtSp3yjh4JdOusGPVwvCn
+
+        def _serviceUrl = "${API_BASE}/v2/store?nearby=$searchCriteria.zip&range=$searchCriteria.radius&key=$consumerKey"
+        println _serviceUrl
+
+        def storesByZip = rest.get(_serviceUrl){
+            accept "application/json"
+            //contentType "application/json"
+        }
+
+        storesByZip.json
+
+    }
+}
