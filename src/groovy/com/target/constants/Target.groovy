56fb1a35837d901a2baebce23847304bb65574a1
dmahapatro <dmahapatro@netjets.com>
2013-07-21 20:51:07 -0400

    COnstanst

---
+++ src/groovy/com/target/constants/Target.groovy
@@ -0,0 +1,15 @@
+package com.target.constants
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Shonu
+ * Date: 7/21/13
+ * Time: 8:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Target {
+    public static final String API_BASE = "https://api.target.com"
+    public static final String consumerKey = "fzg8mSgoVXUxtSp3yjh4JdOusGPVwvCn"
+    public static final String secretKey = "QbndJl7XSMa0ZfXo"
+    public static final String userId = "dmahapatro"
+}
